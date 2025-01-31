3aaf4fe5936b0ee4788f1e3030b078aa9b8c9d9d
Aurelius Prochazka <aure@aure.com>
2025-01-30 21:51:15 -0800

    Added KeychainHelper

---
+++ Sources/CompilerSwiftAI/KeychainHelper.swift
@@ -0,0 +1,51 @@
+//  Copyright © 2025 Compiler, Inc. All rights reserved.
+
+import AuthenticationServices
+import SwiftUI
+
+// Helper for Keychain operations
+public final class KeychainHelper {
+    @MainActor public static let standard = KeychainHelper()
+    public init() {}
+    
+    public func save(_ data: String, service: String, account: String) {
+        guard let data = data.data(using: .utf8) else { return }
+        
+        let query = [
+            kSecValueData: data,
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+        ] as CFDictionary
+        
+        // First remove any existing item
+        SecItemDelete(query)
+        
+        // Add the new item
+        let status = SecItemAdd(query, nil)
+        guard status == errSecSuccess else {
+            print("Error saving to Keychain: \(status)")
+            return
+        }
+    }
+    
+    public func read(service: String, account: String) -> String? {
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecReturnData: true
+        ] as CFDictionary
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query, &result)
+        
+        guard status == errSecSuccess,
+              let data = result as? Data,
+              let string = String(data: data, encoding: .utf8) else {
+            return nil
+        }
+        
+        return string
+    }
+}
