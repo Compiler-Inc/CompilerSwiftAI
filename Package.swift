b5f4935e44fcdb1b4124349390b8f875b8b4df7e
Aurelius Prochazka <aure@aure.com>
2025-02-16 16:04:08 -0800

    Move ChatBubble and dependencies to SDK

--- Package.swift
+++ Package.swift
@@ -6,8 +6,11 @@ let package = Package(
     name: "CompilerSwiftAI",
     platforms: [.macOS(.v15), .iOS(.v18)],
     products: [.library(name: "CompilerSwiftAI", targets: ["CompilerSwiftAI"])],
+    dependencies: [
+        .package(url: "https://github.com/gonzalezreal/swift-markdown-ui", from: "2.4.1"),
+    ],
     targets: [
-        .target(name: "CompilerSwiftAI"),
+        .target(name: "CompilerSwiftAI", dependencies: [.product(name: "MarkdownUI", package: "swift-markdown-ui")] ),
         .testTarget(name: "CompilerSwiftAITests", dependencies: ["CompilerSwiftAI"]),
     ]
 )
