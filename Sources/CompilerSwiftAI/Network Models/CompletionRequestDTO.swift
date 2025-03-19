a723697c2d39e99fb2f9a011089b13b3ec142219
Atharva Vaidya <atharva.vaidya@icloud.com>
2025-03-19 12:32:26 -0700

    Add tool call support

--- Sources/CompilerSwiftAI/Network Models/CompletionRequestDTO.swift
+++ Sources/CompilerSwiftAI/Network Models/CompletionRequestDTO.swift
@@ -71,6 +71,13 @@ struct CompletionRequestDTO: Encodable {
     /// A unique identifier representing your end user.
     let user: String?
     
+    /// Tools (including functions) that the model may call.
+    let tools: [Tool]?
+    
+    /// Controls which (if any) function is called by the model.
+    /// Can be "none", "auto", or a FunctionCall object.
+    let toolChoice: ToolChoice?
+
     // MARK: - Coding Keys
     
     enum CodingKeys: String, CodingKey {
@@ -85,6 +92,8 @@ struct CompletionRequestDTO: Encodable {
         case temperature
         case topP = "top_p"
         case user
+        case tools
+        case toolChoice = "tool_choice"
     }
     
     /// Initialize a new completion request with the given parameters.
@@ -104,7 +113,9 @@ struct CompletionRequestDTO: Encodable {
         stream: Bool? = false,
         temperature: Double? = 1,
         topP: Double? = 1,
-        user: String? = nil
+        user: String? = nil,
+        tools: [Tool]? = nil,
+        toolChoice: ToolChoice? = nil
     ) {
         self.messages = messages
         self.model = model
@@ -122,6 +133,8 @@ struct CompletionRequestDTO: Encodable {
         self.temperature = temperature
         self.topP = topP
         self.user = user
+        self.tools = tools
+        self.toolChoice = toolChoice
     }
     
     func copy(
@@ -140,7 +153,9 @@ struct CompletionRequestDTO: Encodable {
         stream: Bool? = nil,
         temperature: Double? = nil,
         topP: Double? = nil,
-        user: String? = nil
+        user: String? = nil,
+        tools: [Tool]? = nil,
+        toolChoice: ToolChoice? = nil
     ) -> CompletionRequestDTO {
         CompletionRequestDTO(
             messages: messages ?? self.messages,
@@ -158,7 +173,9 @@ struct CompletionRequestDTO: Encodable {
             stream: stream ?? self.stream,
             temperature: temperature ?? self.temperature,
             topP: topP ?? self.topP,
-            user: user ?? self.user
+            user: user ?? self.user,
+            tools: tools ?? self.tools,
+            toolChoice: toolChoice ?? self.toolChoice
         )
     }
 }
@@ -177,6 +194,75 @@ extension CompletionRequestDTO {
                 try container.encode(value)
             case .strings(let values):
                 try container.encode(values)
+            }
+        }
+    }
+}
+
+extension CompletionRequestDTO {
+    /// A tool that can be used by the model.
+    struct Tool: Encodable {
+        /// The type of the tool. Currently, only function is supported.
+        let type: String
+        let function: Function
+        
+        init(function: Function) {
+            self.type = "function"
+            self.function = function
+        }
+    }
+
+    /// A function that can be called by the model.
+    struct Function: Encodable {
+        /// The name of the function to be called. Must be a-z, A-Z, 0-9, or contain underscores and dashes, with a maximum length of 64.
+        let name: String
+        /// A description of what the function does, used by the model to choose when and how to call the function.
+        let description: String?
+        /// The parameters the functions accepts, described as a JSON Schema object.
+        /// Omitting parameters defines a function with an empty parameter list.
+        let parameters: [String: Any]?
+        /// Whether to enable strict schema adherence when generating the function call.
+        /// If set to true, the model will follow the exact schema defined in the parameters field.
+        let strict: Bool?
+
+        enum CodingKeys: String, CodingKey {
+            case name, description, parameters, strict
+        }
+
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(name, forKey: .name)
+            try container.encodeIfPresent(description, forKey: .description)
+            
+            // Encode parameters as raw JSON if provided
+            if let params = parameters {
+                let jsonData = try JSONSerialization.data(withJSONObject: params)
+                let jsonString = String(data: jsonData, encoding: .utf8) ?? "{}"
+                try container.encode(jsonString, forKey: .parameters)
+            }
+            
+            try container.encodeIfPresent(strict, forKey: .strict)
+        }
+    }
+
+    /// Controls which (if any) function is called by the model.
+    enum ToolChoice: Encodable {
+        case none
+        case auto
+        case function(name: String)
+
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.singleValueContainer()
+            switch self {
+            case .none:
+                try container.encode("none")
+            case .auto:
+                try container.encode("auto")
+            case .function(let name):
+                let dict: [String: Any] = ["type": "function", "function": ["name": name]]
+                let jsonData = try JSONSerialization.data(withJSONObject: dict)
+                let jsonString = String(data: jsonData, encoding: .utf8) ?? "{}"
+                try container.encode(jsonString)
             }
         }
     }
